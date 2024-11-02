526e8ed3ee3df0b3f9a552f15d0987bb9e173250
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-11-02 16:57:54 -0400

    scaffold

---
+++ src/main/java/edu/udel/cisc/cisc437/Cisc437Application.java
@@ -0,0 +1,13 @@
+package edu.udel.cisc.cisc437;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cisc437Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cisc437Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/udel/cisc/cisc437/Cisc437ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udel.cisc.cisc437;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cisc437ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
